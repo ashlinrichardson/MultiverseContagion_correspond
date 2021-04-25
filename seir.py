6d92d0733e5730e071de45e973e11f4b6243d6e0
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-04-24 23:30:09 -0700

    update

--- seir.py
+++ seir.py
@@ -1,5 +1,4 @@
-''' adapted from:
-https://towardsdatascience.com/simulating-compartmental-models-in-epidemiology-using-python-jupyter-widgets-8d76bdaff5c2'''
+''' adapted from [1]'''
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
@@ -7,9 +6,7 @@ import scipy.integrate
 odeint = scipy.integrate.odeint
 
 def ode_model(z, t, beta, sigma, gamma): # ODE model
-    """
-    Reference https://www.idmod.org/docs/hiv/model-seir.html
-    """
+    """ Reference https://www.idmod.org/docs/hiv/model-seir.html"""
     S, E, I, R = z
     N = S + E + I + R
     dSdt = -beta*S*I/N
@@ -57,4 +54,10 @@ interact(main,
          mu=FloatSlider(min=0, max=1, step=0.01, value=mu, description='Mortality rate', style=style, layout=slider_layout),
          days=IntSlider(min=1, max=600, step=7, value=days, description='Days', style=style, layout=slider_layout)
         );
+
+references:
+[1] https://towardsdatascience.com/simulating-compartmental-models-in-epidemiology-using-python-jupyter-widgets-8d76bdaff5c2
+[2] https://towardsdatascience.com/infectious-disease-modelling-beyond-the-basic-sir-model-216369c584c4
+[3] https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SEIR_model
+
 '''
