f1ed52c39512a592c013aee8331d1aa76349612e
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-05-31 01:24:45 -0700

    update

---
+++ write_csv_run_nsim.py
@@ -0,0 +1,18 @@
+import os
+def run(c):
+    a = os.system(c)
+    return a
+
+run("mkdir -p n_sim")
+
+for n_sim in [8, 16, 32, 64, 128, 256, 512, 1024]:
+    run("rm *.txt")
+    run("rm *.grep")
+    run("rm *.png")
+    run("rm *.csv")
+    run("python3 write_csv_run.py 500 None None None None 5 " + str(n_sim))
+    run("python3 plot_density.py")
+    run("cp -v mean.csv n_sim/mean_" + str(n_sim) + ".csv")
+    run("cp -v stdv.csv n_sim/stdv_" + str(n_sim) + ".csv")
+    run("cp -v mean_sigma.png n_sim/mean_sigma_" + str(n_sim) + ".png")  
+
