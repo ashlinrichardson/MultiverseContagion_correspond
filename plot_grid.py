4c95fba394ae8846eb39850bf8b1742739ef3957
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-06-22 00:00:03 -0700

    update

--- plot_grid.py
+++ plot_grid.py
@@ -8,12 +8,15 @@ means = [x.strip() for x in os.popen('find ./ -name "mean.csv"')]
 
 dirs = [sep.join(x.split(sep)[:-1]) + sep for x in means]
 sirs = [x + "sir.csv" for x in dirs]
-
-print(sirs)
-sys.exit(1)
+bgr = [x + "beta_gamma_R0.csv" for x in dirs] # beta gamma r0 files..
 
 curves = []
-for m in means:
+for ix in range(len(means)):
+    m = means[ix]
+    d = dirs[ix]
+    print(ix, d)
+    a = os.system("python3 fit_sir.py " + d)
+
     x = [i.strip() for i in open(m).readlines()]
     x = [float(i) for i in x]
     if len(x) < 1:
