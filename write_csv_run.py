7bca5a3935aecc9a600e2d62cb199779fe615bb8
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-06-21 20:24:38 -0700

    update

--- write_csv_run.py
+++ write_csv_run.py
@@ -120,4 +120,6 @@ f.close()
 
 a = os.system('python3 multicore.py simulation_jobs.txt ' + str(multiprocessing.cpu_count())) # let's just add one!
 
+run("python3 plot_density.py") # generate mean curve..
+
 run("mv *.csv *.json *.txt *.png " + dn)
