74b55d675bbb73818bb7b075f3e1afc3ef8a0fc3
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-07-04 14:15:46 -0700

    update

--- write_csv_run_grid.py
+++ write_csv_run_grid.py
@@ -7,10 +7,10 @@ def run(c):
 run("mkdir -p n_sim")
 
 
-n_sim = 8
-for HzR in [0.85 + i for i in range(0, 10)]:
-    for sizeF in [1.5 + (0.5 * i) for i in range(0, 10)]:
-        for mF in [0.75 + (0.5 * i) for i in range(0, 10)]:
+n_sim = 1
+for HzR in [0.85 + i for i in range(0, 20)]:
+    for sizeF in [1. + (0.25 * i) for i in range(0, 20)]:
+        for mF in [0.75 + (0.25 * i) for i in range(0, 20)]:
             run("python3 write_csv_run.py 500 " + str(HzR) + " " +
                                                 str(sizeF) + " " + 
                                                 str(mF) + " " + 
